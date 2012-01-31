ecb47cc7bb6a5f661c77e7f0cded64f872d0b97e
Lauri Helkkula <lhelkkul@gmail.com>
2012-01-31 21:21:42 +0200

    Initial model definition

--- Albumizer/src/Albumizer/albumizer/migrations/0001_initial.py
+++ Albumizer/src/Albumizer/albumizer/migrations/0001_initial.py
@@ -7,29 +7,14 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
-        # Adding model 'Album'
-        db.create_table('albumizer_album', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('albumizer', ['Album'])
+        pass
 
 
     def backwards(self, orm):
-        
-        # Deleting model 'Album'
-        db.delete_table('albumizer_album')
+        pass
 
 
     models = {
-        'albumizer.album': {
-            'Meta': {'object_name': 'Album'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
-        }
     }
 
     complete_apps = ['albumizer']
--- Albumizer/src/Albumizer/albumizer/models.py
+++ Albumizer/src/Albumizer/albumizer/models.py
@@ -2,16 +2,67 @@
 
 from django.db import models
 
-
-
+class User(models.Model):
+    userName=models.CharField(
+        max_length=255)
+    facebookID=models.CharField(
+        max_length=255)
+    
 class Album(models.Model):
-    """ Representes a single album """
+    """ Represents a single album """
 
     title = models.CharField(
        max_length = 255,
        unique = True
     )
     description = models.TextField()
+    isPublic =models.BooleanField()
+    owner =models.ForeignKey(User)
+    
+class Page(models.Model):
+    """Represents a single page"""
+    pageNumber =models.IntegerField()
+    layoutID=models.CharField(
+        max_length= 255)
+    album=models.ForeignKey(Album)
+    
+class PageContent(models.Model):
+    """Represents content in a placeholder"""
+    placeHolderID = models.CharField(
+        max_length=255)
+    content=models.CharField(
+        max_length=255)
+    page=models.ForeignKey(Page)
+    
+
+class Country(models.Model):
+    code=models.CharField(
+        max_length=10)
+    name=models.CharField(
+        max_length=255)
+     
+class Address(models.Model):
+    streetAddress=models.CharField(
+        max_length=255)
+    postOffice=models.CharField(
+        max_length=255)
+    postCode=models.CharField(
+        #10 digits is the maximum length for post codes globally according to Wikipedia
+        max_length=10)    
+    country=models.ForeignKey(Country)
+    
+class Order(models.Model):
+    purchaseDate=models.DateTimeField()
+    status=models.IntegerField()
+    customer=models.ForeignKey(User)
+    
+class OrderItem(models.Model):
+    count=models.IntegerField()
+    order=models.ForeignKey(Order)
+    album=models.ForeignKey(Album)
+    deliveryAddress=models.ForeignKey(Address)
+
+
 
 
 
