c122a27ab566065ff49d2c60c0c160a75d246f02
Lauri Helkkula <lhelkkul@gmail.com>
2012-02-28 02:50:41 +0200

    slide show by google

--- Albumizer/src/Albumizer/albumizer/views.py
+++ Albumizer/src/Albumizer/albumizer/views.py
@@ -231,7 +231,25 @@ def list_all_public_albums(request):
     return render_to_response_as_public('album/list-all.html', RequestContext(request, template_parameters))
 
 
+def view_album_slideshow(request, album_id):
+    myalbum = Album.by_id(album_id)
+    if not myalbum:
+        return render_to_response('album/album-not-found.html', RequestContext(request))
 
+    if myalbum.is_hidden_from_user(request.user):
+        return render_to_response_as_public('album/view-access-denied.html', RequestContext(request))
+
+    layouts=[]
+    for page in myalbum.pages():
+        if not page.layout in layouts:
+            layouts.append(page.layout)
+            
+    templateparameters={
+        "album":myalbum,
+        "layouts":layouts}
+    
+    return render_to_response('album/view-album-slideshow.html', RequestContext(request, templateparameters))
+    
 
 def show_single_page_GET(request, album_id, page_number):
     myalbum = Album.by_id(album_id)
