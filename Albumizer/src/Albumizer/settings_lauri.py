7b783658a79517f7b07f192a2f7da1e8b3639197
Lauri Helkkula <lhelkkul@gmail.com>
2012-02-29 18:56:30 +0200

    another merging issue

--- Albumizer/src/Albumizer/settings_lauri.py
+++ Albumizer/src/Albumizer/settings_lauri.py
@@ -11,3 +11,60 @@ DATABASES = {
       }
     }
 MEDIA_ROOT="d:/Koulu/PhotoAlbumUploads/"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s M%(module)s P%(process)d T%(thread)d -- %(message)s"
+        },
+    },
+    "handlers": {
+        "common_log_file": {
+            "filename": "m:/koulu/AlbumizerLogs/commonlog.txt",
+            "class": "logging.FileHandler",
+            "formatter": "verbose",
+        },
+        "payments_log_file": {
+            "filename": "m:/koulu/AlbumizerLogs/paymentslog.txt",
+            "class": "logging.FileHandler",
+            "formatter": "verbose",
+        },
+        "userActions_log_file": {
+            "filename": "m:/koulu/AlbumizerLogs/useractionlog.txt",
+            "class": "logging.FileHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["common_log_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "django.db.backends": {
+            "level": "INFO",
+            "propagate": True,
+        },
+        "django.request": {
+            "level": "DEBUG",
+            "propagate": True,
+        },
+        "albumizer": {
+            "handlers": ["common_log_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "albumizer.userActions": {
+            "handlers": ["userActions_log_file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "albumizer.payments": {
+            "handlers": ["payments_log_file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    }
+}
